8ef6ecb6cc807a805d8925ad6ebfae4ce95712cf
Chrisplaysharp <cwalden@berkeley.edu>
2017-09-29 04:43:17 -0700

    got it

--- liri.js
+++ liri.js
@@ -31,47 +31,102 @@ function spotifyThis(song) {
 		song = "The Sign";
 	};
 
-	spotify_client.search({ type: 'track', query: song }, function(err, data) {
+	spotify_client.search({ type: 'track', query: song, limit: 1 }, function(err, data) {
 	    if (err) {
 	      console.log('Error occurred: ' + err);
 	      return;
 	    }
-	    var songs = data.tracks.items;
-	    var data = [];
+	    
+	    var result = data.tracks.items[0];
 
-	    for (var i = 0; i < songs.length; i++) {
-	      data.push({
-	        'artist(s)': songs[i].artists.map(artist.name),
-	        'song name: ': songs[i].name,
-	        'preview song: ': songs[i].preview_url,
-	        'album: ': songs[i].album.name,
-	      });
+	    if (!result) {
+	    	console.log("no result found");
+	    } else {
+	    	console.log("Artist: " + result.artists[0].name);
+
+	    	var songTitle = result.name;
+	    	console.log("Song: " + songTitle);
+
+	    	var preview = result.preview_url;
+	    	if (!preview) {
+	    		preview = "No preview listed";
+	    	}
+	    	console.log("Preview link " + preview);
+
+	    	var album = result.album.name;
+	    	if(!album) {
+	    		album = "No album listed"
+	    	}
+	    	console.log("Album: " + album);
 	    }
-	    console.log(data);
   });
 }
 
-function movieThis() {
+function movieThis(movie) {
+	if (movie === undefined) {
+		movie = "Mr Nobody";
+	}
+	//HANDLE two word movies
+	console.log(movie);
+	
+	var queryUrl = 'http://www.omdbapi.com/?t=' + movie + "&apikey=40e9cece";
+	console.log(queryUrl);
 
+	request(queryUrl, function(error, response, body) {
+	    if (error) {
+	    	console.log("Error: " + error.message);
+	    } else {
+	    	var data = [];
+	    	// body = JSON.parse(body);
+
+			data.push({
+				'Title: ' : JSON.parse(body).Title,
+				'Year: ' : JSON.parse(body).Year,
+				'Rated: ' : JSON.parse(body).Rated,
+				'IMDB Rating: ' : JSON.parse(body).imdbRating,
+				'Country: ' : JSON.parse(body).Country,
+				'Language: ' : JSON.parse(body).Language,
+				'Plot: ' : JSON.parse(body).Plot,
+				'Actors: ' : JSON.parse(body).Actors,
+				'Rotten Tomatoes Rating: ' : JSON.parse(body).tomatoRating,
+				'Rotton Tomatoes URL: ' : JSON.parse(body).tomatoURL,
+			});
+			console.log(data);
+		}
+  	});
 }
 
 function doIt() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    var dataArr = data.split(',')
 
+    if (dataArr.length == 2) {
+      run(dataArr[0], dataArr[1]);
+    } else if (dataArr.length == 1) {
+      run(dataArr[0]);
+    }
+
+  });
 }
 
-switch (process.argv[2]) {
-	case "my-tweets":
-		myTweets();
-		break;
-	case "spotify-this-song":
-		spotifyThis(process.argv[3]);
-		break;
-	case "movie-this":
-		movieThis(process.argv[3]);
-		break;
-	case "do-what-it-says":
-		doIt();
-		break;
-	default:
-		console.log("I'm sorry I do not know how to do that");
+function run(instruction, arg) {
+	switch (instruction) {
+		case "my-tweets":
+			myTweets();
+			break;
+		case "spotify-this-song":
+			spotifyThis(arg);
+			break;
+		case "movie-this":
+			movieThis(arg);
+			break;
+		case "do-what-it-says":
+			doIt();
+			break;
+		default:
+			console.log("I'm sorry I do not know how to do that");
+	}
 }
+
+run(process.argv[2], process.argv[3])
